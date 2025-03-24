2d69145992c8674a92657209014fe5d26dfa4210
iamjggrnaut <rustam.tahamtan@gmail.com>
2025-03-24 04:01:49 +0300

    updates

--- src/doctor/doctor.controller.ts
+++ src/doctor/doctor.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { CreateDoctorDto } from "./dto/create-doctor.dto";
 import { DoctorsService } from "./doctor.service";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 
 @Controller("doctors")
 export class DoctorsController {
@@ -12,18 +13,19 @@ export class DoctorsController {
   }
 
   @Post("login")
-  async loginDoc(@Body()input: any) {
+  async loginDoc(@Body() input: any) {
     return this.doctorsService.login(input);
   }
 
-  @Post('find-patient')
-  async getPatientByEmail(@Body() email: string){
-    return this.doctorsService.getPatientByEmail(email)
+  @Post("find-patient")
+  @UseGuards(JwtAuthGuard)
+  async getPatientByEmail(@Body() email: string) {
+    return this.doctorsService.getPatientByEmail(email);
   }
 
-  @Post('assign-patient')
-  async assignPatientToDoctor(@Body() input: object){
-
-    return this.doctorsService.assignPatientToDoctor(input)
+  @Post("assign-patient")
+  @UseGuards(JwtAuthGuard)
+  async assignPatientToDoctor(@Body() input: object) {
+    return this.doctorsService.assignPatientToDoctor(input);
   }
 }
